39c9a67f5c7ddcb900d57c4a88c3acf9b2df262a
Rahul Agrawal <rahulunlimited@gmail.com>
2019-04-25 17:40:53 +1000

    Update powershell-keyvault-adf.ps1

--- scripts/shell/powershell-keyvault-adf.ps1
+++ scripts/shell/powershell-keyvault-adf.ps1
@@ -1,12 +1,9 @@
 #PLEASE UPDATE THE BELOW SECTION
 $RESOURCEGROUP="RG-ADF"
-$PROJECTPREFIX="vlx1"
-
-#Variables. 
-#PLEASE DO NOT UPDATE THIS
+$PROJECTPREFIX="vlr1"
+#Variables. PLEASE DO NOT UPDATE THIS
 $keyVaultName=$PROJECTPREFIX + "kv"
 $dataFactoryName=$PROJECTPREFIX + "adf"
-
 #Update the Key Vault Access Policy for Data Factory
 #***Currently there is no Azure CLI for Data Factory. Once the CLI is available the script can be converted from PowerShell
 Set-AzKeyVaultAccessPolicy -VaultName $keyVaultName -ObjectId (Get-AzDataFactoryV2 -ResourceGroupName $RESOURCEGROUP -Name $dataFactoryName).Identity.PrincipalId -PermissionsToSecrets get
