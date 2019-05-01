ab12f5a01a9b5c2d7cc091f4c7d6f8334833bf1b
Rahul Agrawal <Rahul.Agrawal@velrada.com>
2019-05-01 16:39:46 +1000

    updating sql scripts

--- scripts/sql/02_INITIAL_INSERT.sql
+++ scripts/sql/02_INITIAL_INSERT.sql
@@ -21,23 +21,42 @@ INSERT INTO [UTIL].[DataLoadMetadata] (
 	,[IncrementalLoadColumnType])
 VALUES 
 (@CTR + 1, 'Table', 'Src-OnPrem', 'AdventureWorks2014', 'HumanResources', 'Department', NULL, 'Table',  'STG', 'HumanResources_Department', NULL, 0, NULL, NULL), 
-(@CTR + 2, 'Table', 'Src-OnPrem', 'AdventureWorks2014', 'Production', 'WorkOrder', NULL, 'Table',  'STG', 'Production_WorkOrder', NULL, 1, 'ModifiedDate', 'DATE'), 
-(@CTR + 3, 'Table', 'Src-Azure', 'WideWorldImporters', 'Application', 'Cities', 'SELECT [CityID] ,[CityName] ,[StateProvinceID] ,CONVERT(varbinary(4000), [Location]) [Location] ,LatestRecordedPopulation ,[LastEditedBy] ,[ValidFrom] ,[ValidTo] FROM [Application].[Cities]', 'Table',  'STG', 'Application_Cities', NULL, 0, NULL, NULL),
-(@CTR + 4, 'Table', 'Src-Azure', 'WideWorldImporters', 'Application', 'StateProvinces', 'SELECT S.[StateProvinceID]
-      ,S.[StateProvinceCode]
-      ,S.[StateProvinceName]
-      ,C.IsoAlpha3Code AS CountryCode
-      ,S.[SalesTerritory]
-      ,S.[Border]
-      ,S.[LatestRecordedPopulation]
-  FROM [Application].[StateProvinces] S
-  LEFT JOIN [Application].Countries C ON S.CountryID = C.CountryID', 'Table',  'STG', 'Application_StateProvinces', NULL, 0, NULL, NULL), 
-(@CTR + 5, 'Table', 'Src-Azure', 'WideWorldImporters', 'Application', 'Countries', NULL, 'Table',  'STG', 'Application_Countries', NULL, 0, NULL, NULL), 
-(@CTR + 6, 'Table', 'Src-Azure', 'WideWorldImporters', 'Application', 'DeliveryMethods', NULL, 'Table',  'STG', 'Application_DeliveryMethods', NULL, 0, NULL, NULL),
-(@CTR + 7, 'Table', 'Src-Azure', 'WideWorldImporters', 'Application', 'CitiesNY', 'SELECT  C.*
+(@CTR + 2, 'Table', 'Src-OnPrem', 'AdventureWorks2014', 'Production', 'WorkOrder', NULL, 'Table',  'STG', 'Production_WorkOrder', NULL, 1, 'ModifiedDate', 'DATE') 
+
+GO
+
+DECLARE @CTR INT;
+SELECT @CTR = ISNULL(MAX(RecordID),0) FROM UTIL.DataLoadMetadata
+
+
+INSERT INTO [UTIL].[DataLoadMetadata] (
+	[RecordID]
+	,[SourceType]
+	,[SourceSystem]
+	,[SourceDatabase]
+	,[SourceSchema]
+	,[SourceObject]
+	,[SourceSQL]
+	,[TargetType]
+	,[TargetSchema]
+	,[TargetObject]
+	,[TransformationSP]
+	,[IncrementalLoadFlag]
+	,[IncrementalLoadColumn]
+	,[IncrementalLoadColumnType]
+	,[RunParallel]
+	,[BatchSequence]
+	,[RunSequence])
+VALUES 
+(@CTR + 1, 'Table', 'Src-Azure', 'WideWorldImporters', 'Application', 'Countries', NULL, 'Table',  'STG', 'Application_Countries', 'dbo.sp_spLoad_Country', 0, NULL, NULL, 0, 1, 1), 
+(@CTR + 2, 'Table', 'Src-Azure', 'WideWorldImporters', 'Application', 'StateProvinces', NULL, 'Table',  'STG', 'Application_StateProvinces', 'dbo.spLoad_StateProvince', 0, NULL, NULL, 0, 1, 2), 
+(@CTR + 3, 'Table', 'Src-Azure', 'WideWorldImporters', 'Application', 'Cities', 'SELECT [CityID] ,[CityName] ,[StateProvinceID] ,CONVERT(varbinary(4000), [Location]) [Location] ,LatestRecordedPopulation ,[LastEditedBy] ,[ValidFrom] ,[ValidTo] FROM [Application].[Cities]', 'Table',  'STG', 'Application_Cities', NULL, 0, NULL, NULL, 0, 1, 3),
+(@CTR + 4, 'Table', 'Src-Azure', 'WideWorldImporters', 'Application', 'CitiesNY', 'SELECT  C.*
 FROM [Application].Cities C
 INNER JOIN [Application].StateProvinces S ON S.StateProvinceID = C.StateProvinceID
-WHERE StateProvinceName = ''New York''', 'Table',  'STG', 'Application_Cities_NY', NULL, 0, NULL, NULL)
+WHERE StateProvinceName = ''New York''', 'Table',  'STG', 'Application_Cities_NY', NULL, 0, NULL, NULL, 0, 1, 4),
+(@CTR + 5, 'Table', 'Src-Azure', 'WideWorldImporters', 'Application', 'DeliveryMethods', NULL, 'Table',  'STG', 'Application_DeliveryMethods', NULL, 0, NULL, NULL, 1, 1, 1),
+(@CTR + 6, 'Table', 'Src-Azure', 'WideWorldImporters', 'Application', 'PaymentMethods', NULL, 'Table',  'STG', 'Application_PaymentMethods', NULL, 0, NULL, NULL, 1, 1, 1)
 
 GO
 
@@ -93,7 +112,7 @@ INSERT INTO [UTIL].[DataLoadMetadata] (
 )
 
 VALUES 
-(@CTR + 1, 'File', 'Local', 'AUS-State.csv', 'Table', 'STG', 'CSV_AusState', NULL, ',', '\r\n', 1, 1, 'src', 'dest')
+(@CTR + 1, 'File', 'Local', 'AUS-State.csv', 'Table', 'STG', 'CSV_AusState', 'dbo.spLoad_StateProvince_Ext', ',', '\r\n', 1, 1, 'src', 'dest')
 
 
 
