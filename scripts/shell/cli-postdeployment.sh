fe5544efbe255e979099241053ea8a0a82d87aae
Rahul Agrawal <rahulunlimited@gmail.com>
2020-01-22 14:58:47 +1100

    Update cli-postdeployment.sh

--- scripts/shell/cli-postdeployment.sh
+++ scripts/shell/cli-postdeployment.sh
@@ -2,7 +2,7 @@
 #******PLEASE UPDATE THIS****************
 SUBSCRIPTIONID="ab908bd9-4099-4869-9111-8e2f703691c0"
 RESOURCEGROUP="RG-adflab"
-PROJECTPREFIX="rgadl01"
+PROJECTPREFIX="rgadfl01"
 
 myusername='Rahul.Agrawal@insight.com'
 
