17d90ca8df2f46a63a60855d79321ae32abb35d8
Rahul Agrawal <rahulunlimited@gmail.com>
2020-01-26 15:30:18 +1100

    Updated version manually to Azure12

--- code/database/DataMart/UTIL/Functions/fnGetLocalDateTime.sql
+++ code/database/DataMart/UTIL/Functions/fnGetLocalDateTime.sql
@@ -5,7 +5,7 @@ RETURNS DATETIME
 BEGIN
 
     DECLARE @var DateTime
-    SELECT @var =  CONVERT(DATETIME, GETUTCDATE() AT TIME ZONE 'UTC' AT TIME ZONE 'AUS Eastern Standard Time')
+    SELECT @var =  CONVERT(DATETIME, GETUTCDATE() AT TIME ZONE 'AUS Eastern Standard Time')
    
     RETURN @var
 
